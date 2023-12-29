9ed94b9af7f3e29fbfed724cdfc05368e169f460
zolyomi <jozsef.zolyomi91@gmail.com>
2023-12-29 15:45:43 +0000

    Update MQTTPublishSubscribe.js Layout of the page added

--- src/pages/MQTTPublishSubscribe.js
+++ src/pages/MQTTPublishSubscribe.js
@@ -44,7 +44,53 @@ function MQTTConnectionPanel() {
 function MQTTSubscribePanel() {
   return (
     <div className="card">
-      <div className="card-body">MQTT Broker Subscribe Panel</div>
+      <div className="card-body">
+        MQTT Broker Subscribe Panel and incoming messages
+        <div className="row">
+          <div className="col-sm-5">
+            <label>Subscribe to topic:</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-sm-3">
+            <label>
+              <b>Status</b>
+            </label>
+            <button className="btn btn-primary form-control">Subscribe</button>
+          </div>
+        </div>
+        Subscribed topics:
+        <div className="row">
+          <div className="col-sm-5">
+            <input type="text" className="form-control" disabled />
+          </div>
+          <div className="col-sm-3">
+            <button className="btn btn-danger form-control">UnSubscribe</button>
+          </div>
+        </div>
+        <hr />
+        Incoming messages:
+        <button className="btn btn-sm btn-danger">
+          Clear incoming messages
+        </button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Time</th>
+              <th scope="col">Topic</th>
+              <th scope="col">Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>2023.12.29 16:40</td>
+              <td>test/topic</td>
+              <td>test message. hello world</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
@@ -52,7 +98,26 @@ function MQTTSubscribePanel() {
 function MQTTPublishPanel() {
   return (
     <div className="card">
-      <div className="card-body">MQTT Broker Publish Panel</div>
+      <div className="card-body">
+        MQTT Broker Publish Panel
+        <div className="row">
+          <div className="col-sm-5">
+            <label>Topic:</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-sm-5">
+            <label>Message:</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="col-sm-2">
+            <label>
+              <b>Status</b>
+            </label>
+            <button className="btn btn-primary form-control">Send</button>
+          </div>
+        </div>
+        <p>log</p>
+      </div>
     </div>
   );
 }
